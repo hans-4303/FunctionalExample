6dc3e7d0106811ee78a3d389d2930e5799ccdaab
hans-4303 <103019360+hans-4303@users.noreply.github.com>
2024-01-23 15:30:59 +0900

    불변성 체크

--- FunctionalExample/FunctionalExample.cpp
+++ FunctionalExample/FunctionalExample.cpp
@@ -4,6 +4,18 @@
 #include <vector>
 #include <algorithm>
 
+/// <summary>
+/// <para>
+///     가변 배열 받아서 제곱하는 함수
+/// </para>
+/// </summary>
+/// <param name="input">
+///     타입은 정수를 저장하는 가변 길이 배열인 vector이며
+///     원본 데이터는 참조하되 함수 내에서 변경되지 않음.
+/// </param>
+/// <returns>
+///     람다식을 통해 제곱하며 만들어진 가변 길이 배열 vector
+/// </returns>
 std::vector<int> SquareElements(const std::vector<int>& input) {
     std::vector<int> result;
     std::transform(input.begin(), input.end(), std::back_inserter(result),
--- FunctionalExample/main.cpp
+++ FunctionalExample/main.cpp
@@ -5,6 +5,15 @@ int main() {
     std::vector<int> input = { 1, 2, 3, 4, 5 };
     std::vector<int> squared = SquareElements(input);
 
+    /// <summary>
+    /// 불변성 체크 됨
+    /// </summary>
+    std::cout << "Origin elements: ";
+    for (int value : input) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
     std::cout << "Squared elements: ";
     for (int value : squared) {
         std::cout << value << " ";
